877402e150572cb311122ed1e219af61746c0c3a
denseTurtle <nimapishva@gmail.com>
2021-03-03 15:43:02 -0330

    Update newGame.java

--- Malefiz code/src/newGame.java
+++ Malefiz code/src/newGame.java
@@ -1,49 +1,51 @@
-import java.util.concurrent.Flow.Publisher;
-import java.awt.event.*;
-import java.awt.*;
-import java.swing.*;
-import jPublisher.*
+
 /**
  * 
  */
+import java.util.concurrent.Flow.Publisher;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
 
 /**
  * @author Nima Pishva
  *
  */
-public class newGame extends JFrame, implements ActionListener {
+public class newGame extends JFrame implements ActionListener 
+{
+     
+     public JFrame frame;
+     public JPanel userNamePanel;
+     public JTextField userNameText;
+     public JLabel inputNameLabel;
 
-	 public JFrame frame;
-	 public JPanel userNamePanel;
-	 public JTextField userNameText;
-	public JLabel inputNameLabel;
 
+    public newGame() {
+        
+        frame = new JFrame("User Info");
+        frame.setSize(350, 200);
+        userNamePanel = new JPanel();
+        frame.add(userNamePanel);
+        
+        userNamePanel.setLayout(null);
+        
+        inputNameLabel = new JLabel("Input PlayerName");
+        inputNameLabel.setBounds(10, 20, 80,25);
+        userNamePanel.add(inputNameLabel);
+        
+        userNameText = new JTextField(20);
+        userNameText.setBounds(100, 20, 165, 25);
+        userNamePanel.add(userNameText);
+        frame.setVisible(true);
+    }
+    public void actionPerformed (ActionEvent event){
+    }
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        newGame game = new newGame();
 
-	public newGame() {
-		
-		frame = new JFrame("User Info")
-		frame.setSize(350, 200);
-		userNamePanel = new JPanel();
-		frame.add(userNamePanel);
-		
-		panel.setLayout(null);
-		
-		inputNameLabel = new JLabel("Input PlayerName");
-		inputNameLabel.setBounds(10, 20, 80,25);
-		userNamePanel.add(inputNameLabel);
-		
-		userNameText = new JTextField(20);
-		userNameText.setBounds(100, 20, 165, 25);
-		userNamePanel.add(userNameText);
-		frame.setVisible(true);
-	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		newGame();
-
-	}
+    }
 
 }
