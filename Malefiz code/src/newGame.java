13f785106bd90ed32f6401309ed461888bd632ac
denseTurtle <nimapishva@gmail.com>
2021-03-03 15:02:08 -0330

    is the idea correct?

--- Malefiz code/src/newGame.java
+++ Malefiz code/src/newGame.java
@@ -1,25 +1,48 @@
+import java.util.concurrent.Flow.Publisher;
+import java.awt.event.*;
+import java.awt.*;
+import java.swing.*;
+import jPublisher.*
 /**
  * 
  */
 
 /**
- * @author Fysal Mahmud Rabbe
+ * @author Nima Pishva
  *
  */
-public class newGame {
+public class newGame extends JFrame, implements ActionListener {
 
-	/**
-	 * 
-	 */
+	 public JFrame frame;
+	 public JPanel userNamePanel;
+	 public JTextField userNameText;
+	public JLabel inputNameLabel;
+
+
 	public newGame() {
-		// TODO Auto-generated constructor stub
+		
+		frame = new JFrame("User Info")
+		frame.setSize(350, 200);
+		userNamePanel = new JPanel();
+		frame.add(userNamePanel);
+		
+		panel.setLayout(null);
+		
+		inputNameLabel = new JLabel("Input PlayerName");
+		inputNameLabel.setBounds(10, 20, 80,25);
+		userNamePanel.add(inputNameLabel);
+		
+		userNameText = new JTextField(20);
+		userNameText.setBounds(100, 20, 165, 25);
+		userNamePanel.add(userNameText);
+		frame.setVisible(true);
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		newGame();
 
 	}
 
