f66c8f3421d4816f1fc21f6616a83bbc58a2872e
FysalRabbe <71390639+FysalRabbe@users.noreply.github.com>
2021-03-05 01:09:08 +0600

    changed main

--- Malefiz code/src/chooseTheme.java
+++ Malefiz code/src/chooseTheme.java
@@ -1,4 +1,66 @@
+import java.awt.*;
+import java.awt.Color;
+import java.awt.event.*;
+import javax.swing.*;
+//import java.awt.Cursor;
 
-public class chooseTheme {
-//test101
+public class chooseTheme implements ActionListener,MouseListener
+{
+    JFrame frame = new JFrame("CHOOSE THEME");
+    JPanel info;
+    JButton light,dark;
+    JFrame fra;
+    public chooseTheme(JFrame fr, JPanel panel)
+    {
+       JFrame fra = fr;
+       JPanel subPanel = panel;
+       info = new JPanel();
+       
+       light = new JButton("LIGHT");
+       info.add(light);
+       dark = new JButton("DARK");
+       info.add(dark);
+       dark.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent aActionEvent)
+        {
+            if (aActionEvent.getSource() == dark)
+            {
+              subPanel.setBackground(Color.PINK); 
+              
+            }
+        }
+        });
+       
+       
+       
+       
+       
+       
+       frame.getContentPane().setLayout( new BorderLayout());
+      
+       frame.getContentPane().add(info, BorderLayout.NORTH);
+         
+       frame.pack();
+       frame.setVisible(true);
+       
+       
+    }
+    
+    public void actionPerformed(ActionEvent aevt)
+    {
+        
+    }
+    
+    public void mouseClicked(MouseEvent mevt)
+    {
+        
+    }
+    public void mouseEntered(MouseEvent arg0){}
+    public void mouseExited(MouseEvent arg0) {}
+    public void mousePressed(MouseEvent arg0) {}
+    public void mouseReleased(MouseEvent arg0) {}
+    
+    
+    
+    
 }
--- Malefiz code/src/main.java
+++ Malefiz code/src/main.java
@@ -1,49 +1,109 @@
 //Usually you will require both swing and awt packages
 // even if you are working with just swings.
-import javax.swing.*;
 import java.awt.*;
+//import java.awt.Color;
+import java.awt.event.*;
+import javax.swing.*;
+//import java.awt.Cursor;
 
-public class main {
-    public main() {
+public class main implements ActionListener, MouseListener 
+ {
+    JMenuItem m22, m33, m44;
+    JFrame frame;
+    JPanel mainPanel = new JPanel();
+    public main() 
+    {
 
         //Creating the Frame
         JFrame frame = new JFrame("Malefiz");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(200, 200);
+        frame.setSize(700, 700);
 
         //Creating the MenuBar and adding components
-        JMenuBar mb = new JMenuBar();
-        JMenu m2 = new JMenu("Display Options");
-        mb.add(m2);
-        JMenuItem m22 = new JMenuItem("Choose bightness");
-        JMenuItem m33 = new JMenuItem("Choose cursor");
-        JMenuItem m44 = new JMenuItem("Choose theme");
-        m2.add(m22);
-        m2.add(m33);
-        m2.add(m44);
-       
-
-        //creating buttons
+        JMenuBar menubar = new JMenuBar();
+        JMenu menubar_display = new JMenu("Display Options");
+        menubar_display.addActionListener(this);
+        menubar.add(menubar_display);
+        JMenuItem brightness = new JMenuItem("Choose brightness");
+        JMenuItem cursor = new JMenuItem("Choose cursor");
+        JMenuItem theme = new JMenuItem("Choose theme");
+        
+      //creating buttons
         JButton newGame = new JButton("New Game");
         newGame.setSize(50, 50);
+        
         JButton exitGame = new JButton("Exit Game");
         exitGame.setSize(50, 50);
-        JPanel subPanel = new JPanel();
+        
         JButton loadGame = new JButton("Load Game");
         exitGame.setSize(50, 50);
-        subPanel.add(loadGame);
-        subPanel.add(newGame);
-        subPanel.add(exitGame);
+        
+        mainPanel.add(loadGame);
+        mainPanel.add(newGame);
+        mainPanel.add(exitGame);
+        
+        
+        brightness.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent aActionEvent)
+            {
+              if(aActionEvent.getSource() == brightness)
+              {
+                  //chooseBrightness bright = new chooseBrightness(frame);
+              }  
+            }
+        });
+        menubar_display.add(brightness);
+        cursor.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent aActionEvent)
+            {
+              if(aActionEvent.getSource() == cursor)
+              {
+            	  new chooseCursor(frame);
+               
+              }  
+            }
+        });
+        menubar_display.add(cursor);
+        theme.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent aActionEvent)
+            {
+              if(aActionEvent.getSource() == theme)
+              {
+            	  new chooseTheme(frame, mainPanel);
+              }  
+            }
+        });
+        menubar_display.add(theme);
+       
+
+        
 
 
         //Adding Components to the frame.
-        frame.getContentPane().add(BorderLayout.NORTH, mb);
-        frame.getContentPane().add(BorderLayout.CENTER, subPanel);
+        frame.getContentPane().add(BorderLayout.NORTH, menubar);
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
         frame.setVisible(true);
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
 		// TODO Auto-generated method stub
     	new main();
-	}
+    }
+	
+    public void actionPerformed(ActionEvent aevt)
+    {
+    	
+    }
+    
+    public void mouseClicked(MouseEvent mevt)
+    {
+        //mevt.getSource();
+    	
+    }
+    public void mouseEntered(MouseEvent arg0){}
+    public void mouseExited(MouseEvent arg0) {}
+    public void mousePressed(MouseEvent arg0) {}
+    public void mouseReleased(MouseEvent arg0) {}
+
 }
