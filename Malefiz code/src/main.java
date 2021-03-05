7acd150ba466bca4a6ead517fb2542f54f7dcdd6
FysalRabbe <71390639+FysalRabbe@users.noreply.github.com>
2021-03-06 01:22:14 +0600

    changed

--- Malefiz code/src/main.java
+++ Malefiz code/src/main.java
@@ -73,7 +73,7 @@ public class main implements ActionListener
             {
               if(aActionEvent.getSource() == exitGame)
               {
-            	  //new exitGame();
+            	  new exitGame();
                
               }  
             }
