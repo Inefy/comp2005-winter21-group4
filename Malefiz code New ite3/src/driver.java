67c425a6b7030aecfcc9876d418c40416c41545a
FysalRabbe <71390639+FysalRabbe@users.noreply.github.com>
2021-03-18 03:56:34 +0600

    Update driver.java

--- Malefiz code New ite3/src/driver.java
+++ Malefiz code New ite3/src/driver.java
@@ -2,11 +2,9 @@ import javax.swing.JFrame;
 
 public class driver {
 	
-	public static JFrame frame;
-	
     public static void main(String[] args) 
     {
-    	frame = new JFrame();
+    	JFrame frame = new JFrame();
 		new mainUI(frame);
     }
 
