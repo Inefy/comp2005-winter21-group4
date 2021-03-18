e8cb4044078e2200f418d81a5a12c77a2831c4ea
FysalRabbe <71390639+FysalRabbe@users.noreply.github.com>
2021-03-19 03:06:39 +0600

    added objects

--- Malefiz code New ite3/src/displayBoard.java
+++ Malefiz code New ite3/src/displayBoard.java
@@ -86,6 +86,7 @@ public class displayBoard implements ActionListener{
         
         backPanel.add(boardPanel, BorderLayout.CENTER);
         
+        
         /*while(String.valueOf(step[0][8].getIcon())==String.valueOf(endgoal))
         {
         	//this loop goes on till someone wins the game/reaches the endgoal point
@@ -131,6 +132,7 @@ public class displayBoard implements ActionListener{
 			//valueLabel.setText("Dice Value: "+String.valueOf(endgoal)+"  ");
 			//rollDice.setEnabled(false);
 			turnOrder.setText("Turn: Player "+playerTurn);
+			new movePawn(step,rollDice,diceValue,playerTurn);
 		}
 		
 		if(selected.equals(saveGame)) {
--- Malefiz code New ite3/src/movePawn.java
+++ Malefiz code New ite3/src/movePawn.java
@@ -7,6 +7,14 @@ public class movePawn {
 	public JButton[][] step;
 	public JButton rollDice;
 	public int diceValue, playerTurn;
+	public Icon emptySpace = new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png"));
+	public Icon barricade = new ImageIcon(displayBoard.class.getResource("/images/barricade.png"));
+	public Icon endgoal = new ImageIcon(displayBoard.class.getResource("/images/endgoal.png"));
+	public Icon player1 = new ImageIcon(displayBoard.class.getResource("/images/player1.png"));
+	public Icon player2 = new ImageIcon(displayBoard.class.getResource("/images/player2.png"));
+	public Icon player3 = new ImageIcon(displayBoard.class.getResource("/images/player3.png"));
+	public Icon player4 = new ImageIcon(displayBoard.class.getResource("/images/player4.png"));
+	
 	
 	public movePawn(JButton[][] buttons, JButton rolldice, int dicevalue, int playerturn) {
 		step = buttons;
