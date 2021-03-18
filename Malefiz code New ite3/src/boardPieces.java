cdeb398a3255614ea20832374910ee38a6523321
FysalRabbe <71390639+FysalRabbe@users.noreply.github.com>
2021-03-18 07:01:33 +0600

    Update boardPieces.java

--- Malefiz code New ite3/src/boardPieces.java
+++ Malefiz code New ite3/src/boardPieces.java
@@ -22,127 +22,56 @@ public class boardPieces extends JPanel {
         	for (int j = 0; j < col; j++){
         		if (i==0) {
         			if(j==8) { 
-                    step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
+                    step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/barricade.png")));}
         			}
         		if (i==1) {
-        			if(j ==0||j ==1||j ==2||j ==3||j ==4||j ==5||j ==6||j ==7||j ==8||j ==9||j ==10||j ==11||j ==12||
-        					j ==13||j ==14||j ==15||j ==16) {
-                    step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        		if (i==2) {
-        			if(j ==0||j ==16) {
-                    step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
+        			if(j==8) { 
+                        step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/barricade.png")));}
         			}
         		if (i==3) {
-        			if(j ==0||j ==1||j ==2||j ==3||j ==4||j ==5||j ==6||j ==7||j ==8||j ==9||j ==10||j ==11||j ==12||
-        					j ==13||j ==14||j ==15||j ==16) {
-                    step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
+        			if(j==8) { 
+                        step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/barricade.png")));}
         			}
         		if (i==4) {
-        			if(j ==8) {
-        		    step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
+        			if(j==8) { 
+                        step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/barricade.png")));}
         			}
         		if (i==5) {
-        			if(j ==6||j ==7||j ==8||j ==9||j ==10) {
-        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        		if (i==6) {
-        			if(j ==6||j ==10) {
-        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
+        			if(j==8) { 
+                        step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/barricade.png")));}
         			}
         		if (i==7) {
-        			if(j ==4||j ==5||j ==6||j ==7||j ==8||j ==9||j ==10||j ==11||j ==12) {
-        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        		if (i==8) {
-        			if(j ==4||j ==12) {
-        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        		if (i==9) {
-        			if(j ==2||j ==3||j ==4||j ==5||j ==6||j ==7||j ==8||j ==9||j ==10||j ==11||j ==12||
-        					j ==13||j ==14) {
-        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        		if (i==10) {
-        			if(j ==2|j ==6|j ==10|j ==14) {
-        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
+        			if(j ==6||j ==10) { 
+                        step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/barricade.png")));}
         			}
         		if (i==11) {
-        			if(j ==0||j ==1||j ==2||j ==3||j ==4||j ==5||j ==6||j ==7||j ==8||j ==9||j ==10||j ==11||j ==12||
-        					j ==13||j ==14||j ==15||j ==16) {
-        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        		if (i==12) {
-        			if(j ==0|j ==4|j ==8|j ==12||j ==16) {
-        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        		if (i==13) {
-        			if(j ==0||j ==1||j ==2||j ==3||j ==4||j ==5||j ==6||j ==7||j ==8||j ==9||j ==10||j ==11||j ==12||
-        					j ==13||j ==14||j ==15||j ==16) {
-        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
+        			if(j ==0||j ==4||j ==8||j ==12||j ==16) {
+        				step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/barricade.png")));}
         			}
         		if (i==14) {
-        			if(j ==1||j ==2||j ==3||j ==5||j ==6||j ==7||j ==9||j ==10||j ==11||
-        					j ==13||j ==14||j ==15) {
-        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
+        			if(j ==1||j ==2||j ==3) {
+        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/player1.png")));}
+        			if(j ==5||j ==6||j ==7) {
+        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/player2.png")));}
+        			if(j ==9||j ==10||j ==11) {
+        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/player3.png")));}
+        			if(j ==13||j ==14||j ==15) {
+        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/player4.png")));}
         			}
         		if (i==15) {
-        			if(j ==1||j ==3||j ==5||j ==7||j ==9||j ==11||
-        					j ==13||j ==15) {
-        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
+        			if(j ==1||j ==3) {
+            			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/player1.png")));}
+            			if(j ==5||j ==7) {
+            			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/player2.png")));}
+            			if(j ==9||j ==11) {
+            			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/player3.png")));}
+            			if(j ==13||j ==15) {
+            			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/player4.png")));}
         			}
+        		}
         	}
         }
 		
 		
 	}
 
-}
