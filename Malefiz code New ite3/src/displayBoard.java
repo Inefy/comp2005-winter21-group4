2580d883a4601f890354595df4a99bd624ffe60b
FysalRabbe <71390639+FysalRabbe@users.noreply.github.com>
2021-03-18 21:46:28 +0600

    added setBoard.java

--- Malefiz code New ite3/src/displayBoard.java
+++ Malefiz code New ite3/src/displayBoard.java
@@ -23,11 +23,6 @@ public class displayBoard implements ActionListener{
     	backPanel.removeAll();
     	backPanel.revalidate();
     	backPanel.repaint();
-    	
-    	//frame = new JFrame("board_test");
-        //frame.setSize(700, 700);
-        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame.setResizable(false);
         
     	rollDice = new JButton("Roll Dice");
     	saveGame = new JButton("Save Game");
@@ -63,130 +58,10 @@ public class displayBoard implements ActionListener{
         	}
         }
         
-        for (int i = 0; i < row; i++){
-        	for (int j = 0; j < col; j++){
-        		if (i==0) {
-        			if(j==8) { 
-                    step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        		if (i==1) {
-        			if(j ==0||j ==1||j ==2||j ==3||j ==4||j ==5||j ==6||j ==7||j ==8||j ==9||j ==10||j ==11||j ==12||
-        					j ==13||j ==14||j ==15||j ==16) {
-                    step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        		if (i==2) {
-        			if(j ==0||j ==16) {
-                    step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        		if (i==3) {
-        			if(j ==0||j ==1||j ==2||j ==3||j ==4||j ==5||j ==6||j ==7||j ==8||j ==9||j ==10||j ==11||j ==12||
-        					j ==13||j ==14||j ==15||j ==16) {
-                    step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        		if (i==4) {
-        			if(j ==8) {
-        		    step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        		if (i==5) {
-        			if(j ==6||j ==7||j ==8||j ==9||j ==10) {
-        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        		if (i==6) {
-        			if(j ==6||j ==10) {
-        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        		if (i==7) {
-        			if(j ==4||j ==5||j ==6||j ==7||j ==8||j ==9||j ==10||j ==11||j ==12) {
-        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        		if (i==8) {
-        			if(j ==4||j ==12) {
-        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        		if (i==9) {
-        			if(j ==2||j ==3||j ==4||j ==5||j ==6||j ==7||j ==8||j ==9||j ==10||j ==11||j ==12||
-        					j ==13||j ==14) {
-        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        		if (i==10) {
-        			if(j ==2|j ==6|j ==10|j ==14) {
-        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        		if (i==11) {
-        			if(j ==0||j ==1||j ==2||j ==3||j ==4||j ==5||j ==6||j ==7||j ==8||j ==9||j ==10||j ==11||j ==12||
-        					j ==13||j ==14||j ==15||j ==16) {
-        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        		if (i==12) {
-        			if(j ==0|j ==4|j ==8|j ==12||j ==16) {
-        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        		if (i==13) {
-        			if(j ==0||j ==1||j ==2||j ==3||j ==4||j ==5||j ==6||j ==7||j ==8||j ==9||j ==10||j ==11||j ==12||
-        					j ==13||j ==14||j ==15||j ==16) {
-        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        		if (i==14) {
-        			if(j ==1||j ==2||j ==3||j ==5||j ==6||j ==7||j ==9||j ==10||j ==11||
-        					j ==13||j ==14||j ==15) {
-        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        		if (i==15) {
-        			if(j ==1||j ==3||j ==5||j ==7||j ==9||j ==11||
-        					j ==13||j ==15) {
-        			step[i][j].setIcon(new ImageIcon(displayBoard.class.getResource("/images/emptyspace.png")));
-        			step[i][j].setBorderPainted(false);
-        			step[i][j].setText(null);
-        			step[i][j].setEnabled(true);}
-        			}
-        	}
-        }
-        new boardPieces(boardPanel, step);
+        new setBoard(boardPanel, step);	//sets the empty pieces
+        new boardPieces(boardPanel, step);	//sets pawns to starting position and barricade to its original position
+        
+        
         backPanel.add(boardPanel, BorderLayout.CENTER);
     }
 
