b1422fe91d8a0956c619bf3026f2293a98133e9f
FysalRabbe <71390639+FysalRabbe@users.noreply.github.com>
2021-04-06 00:33:43 +0600

    Update mainUI.java

--- Malefiz code New ite3(use this for now)/src/mainUI.java
+++ Malefiz code New ite3(use this for now)/src/mainUI.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 
 public class mainUI implements ActionListener
  {
-    public JMenuItem cursor, theme;
+    public JMenuItem cursor, theme, instructions;
     public JFrame frame;
     public JPanel mainPanel;
     public JButton newGame;
@@ -26,13 +26,18 @@ public class mainUI implements ActionListener
         frame.setSize(600, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(true);
+        
         //Creating the MenuBar and adding components
         JMenuBar menubar = new JMenuBar();
         JMenu menubar_display = new JMenu("Display Options");
-        //menubar_display.addActionListener(this);
+        JMenu menubar_help = new JMenu("Help");
+        
         menubar.add(menubar_display);
+        menubar.add(menubar_help);
+        
         cursor = new JMenuItem("Choose cursor");
         theme = new JMenuItem("Choose theme");
+        instructions = new JMenuItem("Instructions");
         
         //creating buttons
         JButton newGame = new JButton("New Game");
@@ -128,6 +133,32 @@ public class mainUI implements ActionListener
               if(aActionEvent.getSource() == theme)
               {
             	  new chooseTheme(frame,mainPanel);
+              } 
+              
+            }
+        });
+        
+        menubar_help.add(instructions);
+        instructions.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent aActionEvent)
+            {
+              if(aActionEvent.getSource() == instructions)
+              {
+            	  JOptionPane.showMessageDialog(frame, "> Each player rolls the die takes turn according to the turn order at the bottom left of the board. \r\n"
+            	  		+ "> That player selects one of their five pawns to move the number of spaces shown on the die. \r\n"
+            	  		+ "   They can not select a different pawn after they have selected that pawn.\r\n"
+            	  		+ "> Players can not forgo their turn.\r\n"
+            	  		+ "> The spaces within the players houses are not counted against the die-roll; \r\n"
+            	  		+ "   the first space counted by each pawn is the space immediately above the house. \r\n"
+            	  		+ "> A pawn may pass other pawns of any color, but only one pawn may occupy a given space.\r\n"
+            	  		+ "> In the event that a pawn finishes its move by landing on a space occupied by another pawn, \r\n"
+            	  		+ "   the pawn occupying that space is captured. Captured pawns are returned to their respective \r\n"
+            	  		+ "   houses and may rejoin play on their owner's next turn. \r\n"
+            	  		+ "> Unlike pawns, barricade pieces may not be passed. In order for play to progress past a barricade, \r\n"
+            	  		+ "   the barricade must be captured by a pawn.\r\n"
+            	  		+ "> A player who captures a barricade must relocate the barrier to an unoccupied space on the board. \r\n"
+            	  		+ "   Barricades may not be placed in the four houses or any of the 17 spaces in the bottom-most row. ", 
+      					"Instructions", JOptionPane.WARNING_MESSAGE);
               } 
               
             }
